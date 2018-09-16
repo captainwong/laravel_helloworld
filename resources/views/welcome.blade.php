e8df7e3fd3d53bb28ac122fc3cfdc1b7c8170ed2
vagrant-homestead <qtk.jack@gmail.com>
2018-09-16 07:59:08 +0000

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body{
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                height: 100%;
+                display: table;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                background-color: #fff;
+                color: #636b6f;
+            }
+
+            
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 66px;
+            }
+
+            
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Hello Laravel! - by captainwong</div>
+            </div>
+        </div>
+    </body>
+</html>
